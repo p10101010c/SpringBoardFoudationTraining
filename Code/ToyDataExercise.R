eec45bb9319036710a9495363c8659d3573002e1
p10101010c <p10101010c@gmail.com>
2016-09-22 22:14:27 +1000

    Start

---
+++ Code/ToyDataExercise.R
@@ -0,0 +1,11 @@
+## --------------
+#
+#Author : Naga Bandarupalli
+#Date   : 2016-09-22
+#Purpose: Spring Board Data Science - Data Wrangling Exercise
+#
+## --------------
+
+library("dplyr")
+
+toys_original <- read.csv(file = "Data/refine_original.csv", header = TRUE, sep = ",")
